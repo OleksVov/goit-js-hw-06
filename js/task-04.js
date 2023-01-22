a4fb1ae062354f4a4980772ebad7cbbc8c7ae9a5
Oleksandr <alex.vovchok@gmail.com>
2023-01-22 23:11:54 +0200

    task 8

--- js/task-04.js
+++ js/task-04.js
@@ -10,12 +10,16 @@ const incrementBtn = document.querySelector('button[data-action="increment"]');
 
 const spanWithValue = document.querySelector('#value');
 
-decrementBtn.addEventListener('click', () => {
-counterValue -=1;
-spanWithValue.textContent = `${counterValue}`
-});
+decrementBtn.addEventListener('click', handleButtonDecrement);
 
-incrementBtn.addEventListener('click', () => {
-    counterValue +=1;
-    spanWithValue.textContent = `${counterValue}`
-    });
+incrementBtn.addEventListener('click', handleButtonIncrement);
+
+    function handleButtonDecrement() {
+        counterValue -=1;
+        spanWithValue.textContent = `${counterValue}`
+    };
+
+    function handleButtonIncrement() {
+        counterValue +=1;
+        spanWithValue.textContent = `${counterValue}`
+    };
---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+// Напиши скрипт, который реагирует на изменение значения input#font-size-control (событие input) и изменяет инлайн-стиль span#text обновляя свойство font-size. В результате при перетаскивании ползунка будет меняться размер текста.
+
+const inputSizeText = document.querySelector("#font-size-control");
+
+const spanText = document.querySelector("#text");
+
+inputSizeText.addEventListener("input", () => {
+    spanText.style.fontSize = `${inputSizeText.value}px`;
+});
